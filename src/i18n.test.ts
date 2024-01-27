8f3200b4e8b18e2d917cfd1a1ae35c2cbb22be8c
fsvreddit <john@mclusky.co.uk>
2024-01-27 09:09:29 +0000

    Fix broken test

--- src/i18n.test.ts
+++ src/i18n.test.ts
@@ -1,8 +1,8 @@
-import {localeFromString} from "./i18n.js";
+import {languageFromString} from "./i18n.js";
 
 test("Error occurs when invalid language code passed", () => {
     const t = () => {
-        localeFromString("blah");
+        languageFromString("blah");
     };
 
     expect(t).toThrow("Language code blah not supported");
